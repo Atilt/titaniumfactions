6b696226eacae6fad6985fb203b2edfa09249517
Atilt <angel664410@outlook.com>
2020-04-20 02:22:04 -0700

    improve misc utility

--- src/main/java/com/massivecraft/factions/util/MiscUtil.java
+++ src/main/java/com/massivecraft/factions/util/MiscUtil.java
@@ -3,14 +3,16 @@ package com.massivecraft.factions.util;
 import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.FactionsPlugin;
 import com.massivecraft.factions.perms.Role;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectList;
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
+import it.unimi.dsi.fastutil.objects.ObjectSet;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Creature;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.logging.Level;
 
@@ -45,7 +47,7 @@ public class MiscUtil {
     }
 
     /// TODO create tag whitelist!!
-    public static HashSet<String> substanceChars = new HashSet<>(Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
+    public static ObjectSet<String> substanceChars = new ObjectOpenHashSet<>(Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
 
     public static String getComparisonString(String str) {
         StringBuilder ret = new StringBuilder();
@@ -61,8 +63,8 @@ public class MiscUtil {
         return ret.toString().toLowerCase();
     }
 
-    public static ArrayList<String> validateTag(String str) {
-        ArrayList<String> errors = new ArrayList<>();
+    public static List<String> validateTag(String str) {
+        ObjectList<String> errors = new ObjectArrayList<>();
 
         for (String blacklistItem : FactionsPlugin.getInstance().conf().factions().other().getNameBlacklist()) {
             if (str.toLowerCase().contains(blacklistItem.toLowerCase())) {
@@ -89,11 +91,11 @@ public class MiscUtil {
     }
 
     public static Iterable<FPlayer> rankOrder(Iterable<FPlayer> players) {
-        List<FPlayer> admins = new ArrayList<>();
-        List<FPlayer> coleaders = new ArrayList<>();
-        List<FPlayer> moderators = new ArrayList<>();
-        List<FPlayer> normal = new ArrayList<>();
-        List<FPlayer> recruit = new ArrayList<>();
+        ObjectList<FPlayer> admins = new ObjectArrayList<>();
+        ObjectList<FPlayer> coleaders = new ObjectArrayList<>();
+        ObjectList<FPlayer> moderators = new ObjectArrayList<>();
+        ObjectList<FPlayer> normal = new ObjectArrayList<>();
+        ObjectList<FPlayer> recruit = new ObjectArrayList<>();
 
         for (FPlayer player : players) {
 
@@ -126,7 +128,7 @@ public class MiscUtil {
             }
         }
 
-        List<FPlayer> ret = new ArrayList<>();
+        ObjectList<FPlayer> ret = new ObjectArrayList<>(admins.size() + coleaders.size() + moderators.size() + normal.size() + recruit.size());
         ret.addAll(admins);
         ret.addAll(coleaders);
         ret.addAll(moderators);
